866505886d1a993b43182141a56b538c029d4ed4
Adeodatus <adtsaug@gmail.com>
2021-12-31 17:25:22 +0530

    Custom Settings

--- user.js
+++ user.js
@@ -1214,3 +1214,285 @@ user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
 
 /* END: internal custom pref to test for syntax errors ***/
 user_pref("_user.js.parrot", "SUCCESS: No no he's not dead, he's, he's restin'!");
+
+
+// Preferences
+user_pref("browser.download.dir", "userjs.variable.download");
+user_pref("browser.download.folderList", 2);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.widget.inNavBar", true);
+user_pref("browser.warnOnQuitShortcut", false);
+user_pref("keyword.enabled", true);
+user_pref("middlemouse.contentLoadURL", false);
+user_pref("middlemouse.paste", false);
+user_pref("mousewheel.default.delta_multiplier_y", 100);
+
+// Privacy
+user_pref("app.update.auto", false);
+user_pref("app.update.background.scheduling.enabled", false);
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.contentblocking.category", "custom");
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.search.update", false);
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("dom.vr.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.update.enabled", false);
+user_pref("extensions.update.autoUpdateDefault", false);
+user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("full-screen-api.enabled", true);
+user_pref("geo.enabled", false);
+user_pref("layers.acceleration.disabled", true);
+user_pref("layout.spellcheckDefault", 0);
+user_pref("media.autoplay.blocking_policy", 2);
+user_pref("media.autoplay.default", 5);
+user_pref("media.gmp-provider.enabled", false);
+user_pref("media.peerconnection.enabled", true);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("network.cookie.cookieBehavior", 0);
+user_pref("network.cookie.lifetimePolicy", 2);
+user_pref("network.http.referer.defaultPolicy", 2);
+user_pref("network.http.referer.defaultPolicy.pbmode", 2);
+user_pref("network.http.referer.hideOnionSource", true);
+user_pref("network.http.referer.spoofSource", false);
+user_pref("network.http.referer.trimmingPolicy", 0);
+user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 0);
+user_pref("network.http.sendRefererHeader", 2);
+user_pref("network.trr.mode", 5);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.microphone", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.xr", 2);
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", true);
+user_pref("privacy.clearOnShutdown.formdata", true);
+user_pref("privacy.clearOnShutdown.history", true);
+user_pref("privacy.clearOnShutdown.offlineApps", true);
+user_pref("privacy.clearOnShutdown.sessions", true);
+user_pref("privacy.clearOnShutdown.siteSettings", false);
+user_pref("privacy.donottrackheader.enabled", false);
+user_pref("privacy.firstparty.isolate", false);
+user_pref("privacy.history.custom", true);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("privacy.userContext.enabled", false);
+user_pref("privacy.userContext.ui.enabled", false);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("security.OCSP.enabled", 0);
+user_pref("security.OCSP.require", false);
+user_pref("security.ssl.require_safe_negotiation", false);
+user_pref("signon.generation.enabled", false);
+user_pref("signon.management.page.breach-alerts.enabled", false);
+user_pref("signon.rememberSignons", false);
+
+// Interface
+user_pref("browser.compactmode.show", true);
+user_pref("browser.display.use_document_fonts", 0);
+user_pref("browser.migration.version", 119);
+user_pref("browser.proton.places-tooltip.enabled", true);
+user_pref("browser.tabs.drawInTitlebar", false);
+user_pref("browser.tabs.inTitlebar", 1);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.uidensity", 1);
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"unified-extensions-area\":[\"_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\",\"vimium-c_gdh1995_cn-browser-action\"],\"nav-bar\":[\"urlbar-container\",\"downloads-button\",\"unified-extensions-button\"],\"TabsToolbar\":[\"tabbrowser-tabs\"]},\"seen\":[\"_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\",\"vimium-c_gdh1995_cn-browser-action\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":19,\"newElementCount\":3}");
+user_pref("devtools.editor.keymap", "emacs");
+user_pref("extensions.activeThemeID", "default-theme@mozilla.org");
+user_pref("font.minimum-size.x-western", 16);
+user_pref("font.name.monospace.x-western", "Liberation Mono");
+user_pref("font.name.sans-serif.x-western", "Liberation Sans");
+user_pref("font.name.serif.x-western", "Liberation Serif");
+user_pref("font.size.variable.x-western", 16);
+user_pref("font.size.monospace.x-western", 12);
+user_pref("layout.css.prefers-color-scheme.content-override", 3);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("ui.systemUsesDarkTheme", 1);
+
+// CSS Theme
+user_pref("userChrome.icon.panel_photon", true);
+user_pref("userChrome.icon.panel_sparse", true);
+user_pref("userChrome.rounding.square_tab", true);
+user_pref("userChrome.tab.bottom_rounded_corner", true);
+user_pref("userChrome.tab.box_shadow", true);
+user_pref("userChrome.tab.color_like_toolbar", true);
+user_pref("userChrome.tab.connect_to_window", true);
+user_pref("userChrome.tab.newtab_button_smaller", true);
+user_pref("userChrome.tab.photon_like_padding", true);
+user_pref("userChrome.tab.static_separator", true);
+user_pref("userChrome.tab.photon_like_contextline", true);
+user_pref("userChrome.compatibility.theme", true);
+user_pref("userChrome.compatibility.os", true);
+user_pref("userChrome.theme.built_in_contrast", true);
+user_pref("userChrome.theme.system_default", true);
+user_pref("userChrome.theme.proton_color", true);
+user_pref("userChrome.theme.proton_chrome", true);
+user_pref("userChrome.theme.fully_color", true);
+user_pref("userChrome.theme.fully_dark", true);
+user_pref("userChrome.decoration.cursor", true);
+user_pref("userChrome.decoration.field_border", true);
+user_pref("userChrome.decoration.download_panel", true);
+user_pref("userChrome.decoration.animate", true);
+user_pref("userChrome.padding.tabbar_width", true);
+user_pref("userChrome.padding.tabbar_height", true);
+user_pref("userChrome.padding.toolbar_button", true);
+user_pref("userChrome.padding.navbar_width", true);
+user_pref("userChrome.padding.urlbar", true);
+user_pref("userChrome.padding.bookmarkbar", true);
+user_pref("userChrome.padding.infobar", true);
+user_pref("userChrome.padding.menu", true);
+user_pref("userChrome.padding.bookmark_menu", true);
+user_pref("userChrome.padding.global_menubar", true);
+user_pref("userChrome.padding.panel", true);
+user_pref("userChrome.padding.popup_panel", true);
+user_pref("userChrome.tab.multi_selected", true);
+user_pref("userChrome.tab.unloaded", true);
+user_pref("userChrome.tab.letters_cleary", true);
+user_pref("userChrome.tab.close_button_at_hover", true);
+user_pref("userChrome.tab.sound_hide_label", true);
+user_pref("userChrome.tab.sound_with_favicons", true);
+user_pref("userChrome.tab.pip", true);
+user_pref("userChrome.tab.container", true);
+user_pref("userChrome.tab.crashed", true);
+user_pref("userChrome.fullscreen.overlap", true);
+user_pref("userChrome.icon.library", true);
+user_pref("userChrome.icon.panel", true);
+user_pref("userChrome.icon.menu", true);
+user_pref("userChrome.icon.context_menu", true);
+user_pref("userChrome.icon.global_menu", true);
+user_pref("userChrome.icon.global_menubar", true);
+user_pref("userContent.player.ui", true);
+user_pref("userContent.player.icon", true);
+user_pref("userContent.player.noaudio", true);
+user_pref("userContent.player.size", true);
+user_pref("userContent.player.click_to_play", true);
+user_pref("userContent.player.animate", true);
+user_pref("userContent.newTab.field_border", true);
+user_pref("userContent.newTab.full_icon", true);
+user_pref("userContent.newTab.animate", true);
+user_pref("userContent.newTab.pocket_to_last", true);
+user_pref("userContent.newTab.searchbar", true);
+user_pref("userContent.page.illustration", true);
+user_pref("userContent.page.proton_color", true);
+user_pref("userContent.page.dark_mode", true);
+user_pref("userContent.page.proton", true);
+// user_pref("userChrome.compatibility.covered_header_image", true);
+// user_pref("userChrome.compatibility.panel_cutoff",         true);
+// user_pref("userChrome.compatibility.navbar_top_border",    true);
+// user_pref("userChrome.compatibility.dynamic_separator",    true); // Need dynamic_seperator
+// user_pref("userChrome.compatiblity.os.linux_non_native_titlebar_button", true);
+// user_pref("userChrome.decoration.disable_sidebar_animate",  true);
+// user_pref("userChrome.theme.proton_color.dark_blue_accent", true);
+// user_pref("userChrome.rounding.square_button",              true);
+// user_pref("userChrome.rounding.square_panel",               true);
+// user_pref("userChrome.rounding.square_panelitem",           true);
+// user_pref("userChrome.rounding.square_menupopup",           true);
+// user_pref("userChrome.rounding.square_menuitem",            true);
+// user_pref("userChrome.rounding.square_field",               true);
+// user_pref("userChrome.rounding.square_checklabel",          true);
+// user_pref("userChrome.padding.first_tab",                   true);
+// user_pref("userChrome.padding.drag_space",                  true);
+// user_pref("userChrome.padding.menu_compact",                true);
+// user_pref("userChrome.padding.urlView_expanding",           true);
+// user_pref("userChrome.padding.urlView_result",              true);
+// user_pref("userChrome.padding.panel_header",                true);
+// user_pref("userChrome.urlView.move_icon_to_left",           true);
+// user_pref("userChrome.urlView.go_button_when_typing",       true);
+// user_pref("userChrome.urlView.always_show_page_actions",    true);
+// user_pref("userChrome.tab.on_bottom",                       true);
+// user_pref("userChrome.tab.on_bottom.above_bookmark",        true); // Need on_bottom
+// user_pref("userChrome.tab.on_bottom.menubar_on_top",        true); // Need on_bottom
+// user_pref("userChrome.tab.always_show_tab_icon",            true);
+// user_pref("userChrome.tab.close_button_at_pinned",          true);
+// user_pref("userChrome.tab.close_button_at_hover.always",    true); // Need close_button_at_hover
+// user_pref("userChrome.tab.sound_show_label",                true); // Need remove sound_hide_label
+// user_pref("userChrome.tab.centered_label",                  true);
+// user_pref("userChrome.panel.remove_strip",                  true);
+// user_pref("userChrome.panel.full_width_separator",          true);
+// user_pref("userChrome.panel.full_width_padding",            true);
+// user_pref("userChrome.icon.account_image_to_right",         true);
+// user_pref("userChrome.icon.account_label_to_right",         true);
+// -- User Content -------------------------------------------------------------
+// user_pref("userContent.player.ui.twoline",                  true);
+// user_pref("userContent.page.proton_color.dark_blue_accent", true);
+// user_pref("userContent.page.proton_color.system_accent",    true);
+// == Only Sharpen Scrolling ===================================================
+//         Pref                                             Value      Original
+/*
+user_pref("mousewheel.min_line_scroll_amount",                 10); //        5
+user_pref("general.smoothScroll.mouseWheel.durationMinMS",     80); //       50
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); //   "0.25"
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); //    "0.4"
+*/
+// == Smooth Scroling ==========================================================
+// ** Scrolling Options ********************************************************
+// based on natural smooth scrolling v2 by aveyo
+// this preset will reset couple extra variables for consistency
+//         Pref                                              Value                 Original
+/*
+user_pref("apz.allow_zooming",                               true);            ///     true
+user_pref("apz.force_disable_desktop_zooming_scrollbars",   false);            ///    false
+user_pref("apz.paint_skipping.enabled",                      true);            ///     true
+user_pref("apz.windows.use_direct_manipulation",             true);            ///     true
+user_pref("dom.event.wheel-deltaMode-lines.always-disabled", true);            ///    false
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.12");            ///   "0.25" <- 1. If scroll too slow, set to "0.15"
+user_pref("general.smoothScroll.durationToIntervalRatio",    1000);            ///      200
+user_pref("general.smoothScroll.lines.durationMaxMS",         100);            ///      150
+user_pref("general.smoothScroll.lines.durationMinMS",           0);            ///      150
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS",    100);            ///      200
+user_pref("general.smoothScroll.mouseWheel.durationMinMS",      0);            ///       50
+user_pref("general.smoothScroll.mouseWheel.migrationPercent", 100);            ///      100
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);   ///      120
+user_pref("general.smoothScroll.msdPhysics.enabled",                  true);   ///    false
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 200);   ///     1250
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",     200);   ///     1000
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         10);   ///       12
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",  "1.20");   ///    "1.3"
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",   1000);   ///     2000
+user_pref("general.smoothScroll.other.durationMaxMS",         100);            ///      150
+user_pref("general.smoothScroll.other.durationMinMS",           0);            ///      150
+user_pref("general.smoothScroll.pages.durationMaxMS",         100);            ///      150
+user_pref("general.smoothScroll.pages.durationMinMS",           0);            ///      150
+user_pref("general.smoothScroll.pixels.durationMaxMS",        100);            ///      150
+user_pref("general.smoothScroll.pixels.durationMinMS",          0);            ///      150
+user_pref("general.smoothScroll.scrollbars.durationMaxMS",    100);            ///      150
+user_pref("general.smoothScroll.scrollbars.durationMinMS",      0);            ///      150
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6");            ///    "0.4"
+user_pref("layers.async-pan-zoom.enabled",                   true);            ///     true
+user_pref("layout.css.scroll-behavior.spring-constant",   "250.0");            ///   "250.0"
+user_pref("mousewheel.acceleration.factor",                     3);            ///       10
+user_pref("mousewheel.acceleration.start",                     -1);            ///       -1
+user_pref("mousewheel.default.delta_multiplier_x",            100);            ///      100
+user_pref("mousewheel.default.delta_multiplier_y",            100);            ///      100
+user_pref("mousewheel.default.delta_multiplier_z",            100);            ///      100
+user_pref("mousewheel.min_line_scroll_amount",                  0);            ///        5
+user_pref("mousewheel.system_scroll_override.enabled",       true);            ///     true <- 2. If scroll too fast, set to false
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", false); ///     true
+user_pref("mousewheel.transaction.timeout",                  1500);            ///     1500
+user_pref("toolkit.scrollbox.horizontalScrollDistance",         4);            ///        5
+user_pref("toolkit.scrollbox.verticalScrollDistance",           3);            ///        3
+*/
